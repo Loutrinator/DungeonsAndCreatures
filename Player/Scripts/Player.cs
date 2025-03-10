3dbb30a592414c858edfae8dcf356fb36d7d4d97
Antoine <antoine.pavy92@gmail.com>
2025-03-10 23:12:41 +0100

    WIP tileset

--- Player/Scripts/Player.cs
+++ Player/Scripts/Player.cs
@@ -5,19 +5,32 @@ public partial class Player : CharacterBody2D
 {
 	[Export]
 	private float _moveSpeed = 100.0f;
+
+	[Export]
+	private AnimationPlayer _animationPlayer = null;
+	[Export]
+	private Sprite2D _sprite2D = null;
+
+	private Vector2 _direction = new Vector2(0,0);
+	private String state = "Idle";
 	
 	public override void _Ready()
 	{
 		int i = 42;
+		_animationPlayer.Play("Idle");
 	}
 	
-	public override void _Process(double delta){
-		Vector2 direction = new Vector2(0,0);
-		direction.X = Input.GetActionStrength("right") - Input.GetActionStrength("left");
-		direction.Y = Input.GetActionStrength("down") - Input.GetActionStrength("up");
+	public override void _Process(double delta)
+	{
+		_direction.X = Input.GetActionStrength("right") - Input.GetActionStrength("left");
+		_direction.Y = Input.GetActionStrength("down") - Input.GetActionStrength("up");
 		
-		Velocity = direction.Normalized() * _moveSpeed;
-
+		Velocity = _direction.Normalized() * _moveSpeed;
+		UpdateSpriteDirection();
+		if(UpdateState())
+		{
+			UpdateAnimation();
+		}
 		//direction.x = Input.GetActionStrength();
 	}
 
@@ -25,4 +38,28 @@ public partial class Player : CharacterBody2D
     {
 		MoveAndSlide();
     }
+
+	private bool SetDirection()
+	{
+		return true;
+	}
+	private bool UpdateState()
+	{
+		String newState = _direction == Vector2.Zero ? "Idle" : "Run";
+		if(newState == state){
+			return false;
+		}
+		state = newState;
+		return true;
+	}
+
+	private void UpdateAnimation()
+	{
+		_animationPlayer.Play(state);
+	}
+
+	public void UpdateSpriteDirection()
+	{
+		_sprite2D.FlipH = _direction.X < 0;
+	}
 }
